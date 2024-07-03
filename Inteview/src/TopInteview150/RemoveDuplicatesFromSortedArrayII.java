1fa68eec9852b9ed15a05ab67511b7d861ccb23a
Jinxu Zheng <1710198939@qq.com>
2024-07-02 22:21:38 -0700

    review code

--- Inteview/src/TopInteview150/RemoveDuplicatesFromSortedArrayII.java
+++ Inteview/src/TopInteview150/RemoveDuplicatesFromSortedArrayII.java
@@ -4,12 +4,17 @@ package TopInteview150;
  * @Author Jinxu Zheng
  * @Date 1/24/2024
  **/
+
 public class RemoveDuplicatesFromSortedArrayII {
-    public int removeDuplicates(int[] nums) {
+    public static void main(String[]args){
+        int[] ints = {1, 1, 1, 2, 2, 3};
+        int i = removeDuplicates(ints);
+        System.out.println(i);
+    }
+    public static int removeDuplicates(int[] nums) {
         int j = 2;
         for (int i = 2; i < nums.length; i++) {
             if (nums[i] != nums[j - 2]) {
-
                 nums[j] = nums[i];
                 j++;
             }
