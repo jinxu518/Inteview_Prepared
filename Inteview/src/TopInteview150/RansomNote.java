5b946a2323f8a5a226881ed60f57edfc3b39833d
Jinxu Zheng <1710198939@qq.com>
2024-07-06 21:52:35 -0700

    practice

---
+++ Inteview/src/TopInteview150/RansomNote.java
@@ -0,0 +1,31 @@
+package TopInteview150;
+
+import java.util.HashMap;
+
+/**
+ * @Author Jinxu Zheng
+ * @Date 7/6/2024
+ **/
+public class RansomNote {
+    public boolean canConstruct(String r, String m) {
+        if (r.length() > m.length()) return false;
+        HashMap<Character, Integer> rMap = new HashMap<>();
+
+        for (char c : r.toCharArray()) {
+            rMap.put(c, rMap.getOrDefault(c, 0) + 1);
+        }
+        int co=rMap.size();
+        for(char c : m.toCharArray()){
+
+            if(rMap.containsKey(c)){
+                rMap.put(c,rMap.get(c)-1);
+                if(rMap.get(c)==0) {
+                    rMap.remove(c);
+                    co--;
+                }
+                if(co==0) return true;
+            }
+        }
+        return false;
+    }
+}
