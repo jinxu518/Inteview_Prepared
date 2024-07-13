492c158a515fb073782d3d3aa89e14fc93c0fdf1
Jinxu Zheng <1710198939@qq.com>
2024-07-12 17:58:14 -0700

    practice

---
+++ Inteview/src/TopInteview150/HappyNumber.java
@@ -0,0 +1,26 @@
+package TopInteview150;
+
+/**
+ * @Author Jinxu Zheng
+ * @Date 7/12/2024
+ **/
+public class HappyNumber {
+    public boolean isHappy(int n) {
+        if (n == 1 || n == 7) {
+            return true;
+        }
+        while (n > 9) {
+            int sum = 0;
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+            n = sum;
+            if (n == 1 || n == 7) {
+                return true;
+            }
+        }
+        return n == 1 || n == 7;
+    }
+}
