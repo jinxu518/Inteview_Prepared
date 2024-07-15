d3109ce604511974ebcd5d5d76e523823238579e
Jinxu Zheng <1710198939@qq.com>
2024-07-14 23:40:59 -0700

    practice

---
+++ Inteview/src/TopInteview150/ContainsDuplicateII.java
@@ -0,0 +1,23 @@
+package TopInteview150;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author Jinxu Zheng
+ * @Date 7/14/2024
+ **/
+public class ContainsDuplicateII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                if ((i - map.get(nums[i])) <= k) {
+                    return true;
+                }
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
