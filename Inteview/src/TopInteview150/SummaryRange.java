fa6ed2d84e13cf261099bbf4f91822af797ca561
Jinxu Zheng <1710198939@qq.com>
2024-07-15 22:54:52 -0700

    practice

---
+++ Inteview/src/TopInteview150/SummaryRange.java
@@ -0,0 +1,32 @@
+package TopInteview150;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * @Author Jinxu Zheng
+ * @Date 7/15/2024
+ **/
+public class SummaryRange {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        if (nums.length == 0) {
+            return result;
+        }
+        int start = nums[0];
+        for (int i = 1; i <= nums.length; i++) {
+            if (i < nums.length && nums[i] == nums[i - 1] + 1) {
+                continue;
+            }
+            if (start == nums[i - 1]) {
+                result.add(String.valueOf(start));
+            } else {
+                result.add(start + "->" + nums[i - 1]);
+            }
+            if (i < nums.length) {
+                start = nums[i];
+            }
+        }
+        return result;
+    }
+}
